dc0cf9d75e021d5743d5c3986d5fa80307530fcc
5and3Eduardo <5and3eduardo@gmail.com>
2021-04-07 12:08:29 +0100

    second commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="content antialiased">
+    <Header></Header>
+  
+    <Welcome></Welcome>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Welcome from './components/Welcome.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Welcome,
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style>
+
+</style>
